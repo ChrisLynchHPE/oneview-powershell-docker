0e34ea6b75daf103748657f263f090042832e631
bryansullins <bryan.sullins@gmail.com>
2021-09-01 22:20:25 -0700

    Cleaned up order and comments.

--- release/ubuntu20.04/build.ps1
+++ release/ubuntu20.04/build.ps1
@@ -13,17 +13,16 @@ param(
         [bool]$KeepPreviousImageTagName
     )
 
-# Desired Local Tag Name
-# $ImageTagName='oneview-powershell-ubuntu'
+# Uncomment to Override $ImageTageName specified.
+# $ImageTagName='oneview-powershell-ubuntu' # Example
 
-# If there is an existing image by that name and user does not want to keep previous image, delete any existing image by this $ImageTagName
-$ImageExists = docker image list | grep $ImageTagName
-If (($KeepPreviousImageTagName -eq $false) -And ($ImageExists)) {
-    docker rmi --force $ImageTagName
-}
-
-# Ensure proper entry for HPE Powershell Version and run if using a proper version:
+# Ensure proper entry for HPE Powershell Version and run only if using a proper version:
 If (($HPEOVVersion -eq '5.50') -Or ($HPEOVVersion -eq '6.00') -Or ($HPEOVVersion -eq '6.10')) {
+    # If there is an existing image by that name and user does not want to keep previous image, delete any existing image by this $ImageTagName
+    $ImageExists = docker image list | grep $ImageTagName
+    If (($KeepPreviousImageTagName -eq $false) -And ($ImageExists)) {
+      docker rmi --force $ImageTagName
+    }
     # Setup Parameters for use with docker command.
     $Env:OVMajorVersion=$HPEOVVersion.split(".") | Select-Object -Index 0
     $Env:OVMinorVersion=$HPEOVVersion.split(".") | Select-Object -Index 1
@@ -36,6 +35,6 @@ If (($HPEOVVersion -eq '5.50') -Or ($HPEOVVersion -eq '6.00') -Or ($HPEOVVersion
 
 }
 Else {
-    Write-Host -ForegroundColor Red  "You must enter a Version that is 5.50, 6.00, or 6.10 at this time or the docker build will fail. If there is a newer version please contact bryan.sullins@gmail.com or alter the PS code in the build.ps1 file."
+    Write-Host -ForegroundColor Red  "You must enter an HPEOVVersion that is 5.50, 6.00, or 6.10 at this time or the docker build will fail. If there is a newer version please contact bryan.sullins@gmail.com or alter the PS code in the build.ps1 file."
     Exit
 }
